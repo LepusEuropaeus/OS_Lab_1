c5362867552f03c39c031f98bcb968d2e4eaf093
LepusEuropaeus <igor.zaitsev.1337@gmail.com>
2024-02-11 18:02:40 +0300

    Добавьте файлы проекта.

---
+++ Functions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace Lab_1
+{
+     public static class Functions
+    {
+        public static double Square(double x)
+        {
+            return x * x;
+        }
+
+        public static double Cube(double x)
+        {
+            return x * x * x;
+        }
+
+        public static double SquareRoot(double x)
+        {
+            if(x< 0)
+            {
+                MessageBox.Show("Введите число больше нуля");
+                return double.NaN;
+            }
+
+            return Math.Sqrt(x);
+        }
+
+        public static double SomePolynomial(double x)
+        {
+            return Math.Pow(x, 4) + 24 * Math.Pow(x, 3) - 5 * Math.Pow(x, 2) + 10 * x - 623;
+        }
+
+        public static double Choosing(int func, double x)
+        {
+            switch (func)
+            {
+                case 0:
+                    return Square(x);
+                case 1:
+                    return Cube(x);
+                case 2: 
+                    return SquareRoot(x);
+                case 3: 
+                    return SomePolynomial(x);
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+        public static List<string> FunctionsList = new List<string>() { "x^2", "x^3", "sqrt(x)", "x^4 + 24x^3 - 5x^2 + 10x - 623" };
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random rnd = new Random();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ChooseFunction.ItemsSource = Functions.FunctionsList;
+        }
+
+        private void CalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (double.TryParse(CalculationTextbox.Text, out double num))
+                {
+                    AnswerTextbox.Text = Convert.ToString(Functions.Choosing(ChooseFunction.SelectedIndex, num));
+                }
+                else
+                {
+                    MessageBox.Show("Необходимо ввести число");
+                    CalculationTextbox.Clear();
+                }
+            }
+            catch (NotImplementedException)
+            {
+                MessageBox.Show("Выберите функцию");
+            }
+        }
+
+        private void Task2Left_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Task2Left.Text = "Пришел";
+            Task2Right.Text = "Ушел";
+        }
+
+        private void Task2Right_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Task2Left.Text = "Ушел";
+            Task2Right.Text = "Пришел";
+        }
+
+        private void Task2Left_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Task2Left.Text = "";
+            Task2Right.Text = "";
+        }
+
+        private void Grid_MouseMove(object sender, MouseEventArgs e)
+        {
+            Colorful.Background = new SolidColorBrush(Color.FromRgb((byte)rnd.Next(255), (byte)rnd.Next(255), (byte)rnd.Next(255)));
+        }
+    }
+}
